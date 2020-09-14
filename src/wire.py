7fc0ead4f91ed82addfb33f1f6e9dc0a806ec8e7
Mike Walshe <mjwalshe@gmail.com>
2020-09-14 16:58:41 +0100

    Add output

--- src/wire.py
+++ src/wire.py
@@ -1,3 +1,4 @@
 class Wire():
     def __init__(self):
         self.end_a_conn = None
+        self.end_b_conn = None
--- tests/test_wire.py
+++ tests/test_wire.py
@@ -1,7 +1,16 @@
 import pytest
 from src import wire
 
+@pytest.fixture(autouse=True)   
+def before_wire():
+    test_wire = wire.Wire()
+    return test_wire
+
 class TestInitialization:
-    def testInput(self):
-        test_wire = wire.Wire()
+    def testInput(self, before_wire):
+        test_wire = before_wire
         assert test_wire.end_a_conn == None
+
+    def testOutput(self, before_wire):
+        test_wire = before_wire
+        assert test_wire.end_b_conn == None
