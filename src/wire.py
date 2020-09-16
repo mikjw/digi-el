a8f23f2dc3c0cc5b4cc50d63065de9dcecd44ff5
Mike Walshe <mjwalshe@gmail.com>
2020-09-16 14:13:50 +0100

    Add basic connect_next method

--- src/wire.py
+++ src/wire.py
@@ -2,3 +2,6 @@ class Wire():
     def __init__(self):
         self.in_conn = None
         self.out_conn = None
+
+    def connect_next(self, component):
+        self.out_conn = component
--- tests/test_wire.py
+++ tests/test_wire.py
@@ -6,6 +6,11 @@ def before_wire():
     test_wire = wire.Wire()
     return test_wire
 
+@pytest.fixture(autouse=True)   
+def before_wire_2():
+    test_wire_2 = wire.Wire()
+    return test_wire_2
+
 class TestInitialization:
     def testInput(self, before_wire):
         test_wire = before_wire
@@ -14,3 +19,10 @@ class TestInitialization:
     def testOutput(self, before_wire):
         test_wire = before_wire
         assert test_wire.out_conn == None
+
+class TestConnection:
+    def test_connects_to_next_component(self, before_wire, before_wire_2):
+        test_wire = before_wire
+        test_wire_2 = before_wire_2
+        test_wire.connect_next(test_wire_2)
+        assert test_wire.out_conn == test_wire_2
