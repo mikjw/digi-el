bb6ab21072adc337a8c1c1bbf114a8eed98ee186
Mike <mjwalshe@gmail.com>
2020-10-17 06:53:31 +0100

    Add input and output counts

--- src/container.py
+++ src/container.py
@@ -1,4 +1,6 @@
 class Container():
     def __init__(self):
+        self.input_count = 1
+        self.output_count = 1
         self.inputs = {'A': {'component': None, 'signal': None}}
         self.outputs = {'A': {'component': None, 'signal': None}}
--- tests/test_container.py
+++ tests/test_container.py
@@ -8,6 +8,13 @@ def test_container():
 class TestInitialization:
     def test_input(self, test_container):
         assert test_container.inputs['A'] == {'component': None, 'signal': None}
-
+        
     def test_output(self, test_container):
         assert test_container.outputs['A'] == {'component': None, 'signal': None}
+        
+    def test_inits_with_input_count(self, test_container):
+        assert test_container.input_count == 1
+        
+    def test_inits_with_output_count(self, test_container):
+        assert test_container.output_count == 1
+               
