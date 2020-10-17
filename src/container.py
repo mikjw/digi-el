97cfdf857134abbe5fe0b7fef0949ee02f0bf6cc
Mike <mjwalshe@gmail.com>
2020-10-17 06:38:04 +0100

    Add outputs dict

--- src/container.py
+++ src/container.py
@@ -1,3 +1,4 @@
 class Container():
     def __init__(self):
         self.inputs = {'A': {'component': None, 'signal': None}}
+        self.outputs = {'A': {'component': None, 'signal': None}}
--- tests/test_container.py
+++ tests/test_container.py
@@ -6,5 +6,8 @@ def test_container():
     return container.Container()
 
 class TestInitialization:
-    def test_input_connection(self, test_container):
+    def test_input(self, test_container):
         assert test_container.inputs['A'] == {'component': None, 'signal': None}
+
+    def test_output(self, test_container):
+        assert test_container.outputs['A'] == {'component': None, 'signal': None}
