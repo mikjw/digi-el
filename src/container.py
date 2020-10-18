781c1388cb47761bb17c1763680eec7a680702b2
Mike <mjwalshe@gmail.com>
2020-10-18 09:46:19 +0100

    Refactor add_input

--- src/container.py
+++ src/container.py
@@ -6,16 +6,7 @@ class Container():
         self.outputs = {'A': {'component': None, 'signal': None}}
         
     def add_input(self):
-        if 'D' in self.inputs:
-            self.inputs['E'] = {'component': None, 'signal': None}
-            self.input_count = 5
-        elif 'C' in self.inputs:
-            self.inputs['D'] = {'component': None, 'signal': None}
-            self.input_count = 4  
-        elif 'B' in self.inputs:
-            self.inputs['C'] = {'component': None, 'signal': None}
-            self.input_count = 3  
-        else:
-            self.inputs['B'] = {'component': None, 'signal': None} 
-            self.input_count = 2
+        self.input_count += 1
+        label = chr(self.input_count + 64)
+        self.inputs[label] = {'component': None, 'signal': None}
 
