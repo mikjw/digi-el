98de1cf52cd5b5b2d7511154210f83ab8195337e
Mike Walshe <mjwalshe@gmail.com>
2020-11-29 19:58:50 +0000

    Add printer class

---
+++ tests/test_printer.py
@@ -0,0 +1,10 @@
+import pytest
+from src import printer
+
+@pytest.fixture(autouse=True)   
+def test_printer():
+    return printer.Printer()
+
+class TestInitialization:
+    def test_has_empty_inputs_dict(self, test_printer):
+        assert test_printer.inputs == {}
