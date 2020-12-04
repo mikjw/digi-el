70be14236a49c6ca77b2d52d0036bc2e43176df6
Mike Walshe <mjwalshe@gmail.com>
2020-12-04 16:41:48 +0000

    Add third call

--- tests/test_printer.py
+++ tests/test_printer.py
@@ -42,3 +42,20 @@ class TestInputCreation:
                 test_printer.add_input()
             test_printer.inputs['C']
 
+    def test_adds_input_c(self, test_printer):
+        for i in range(3):
+            test_printer.add_input()
+        assert test_printer.inputs['C'] == {'component': None, 'signal': None}
+
+    def test_increments_input_counter_to_3(self, test_printer):
+        for i in range(3):
+            test_printer.add_input()
+        assert test_printer.input_count == 3
+
+    def test_does_not_add_more_than_three_inputs(self, test_printer):
+        with pytest.raises(KeyError):
+            for i in range(3):
+                test_printer.add_input()
+            test_printer.inputs['D']
+
+
