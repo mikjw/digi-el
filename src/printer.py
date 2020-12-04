437fd1bbf912cd7a03ee8efd3d19082b91913a75
Mike Walshe <mjwalshe@gmail.com>
2020-12-04 16:25:17 +0000

    Add input creation

--- src/printer.py
+++ src/printer.py
@@ -3,5 +3,10 @@ class Printer():
         self.input_count = 0
         self.inputs = {}
 
+    def add_input(self):
+        self.inputs['A'] = {'component': None, 'signal': None}
+        self.inputs['B'] = {'component': None, 'signal': None}
+        self.inputs['C'] = {'component': None, 'signal': None}
+        self.input_count += 1
 
 
--- tests/test_printer.py
+++ tests/test_printer.py
@@ -12,4 +12,22 @@ class TestInitialization:
     def test_has_input_count(self, test_printer):
         assert test_printer.input_count == 0
 
+class TestInputCreation:
+    def test_adds_input_a(self, test_printer):
+        test_printer.add_input()
+        assert test_printer.inputs['A'] == {'component': None, 'signal': None}
+
+    def test_increments_input_counter_to_1(self, test_printer):
+        test_printer.add_input()
+        assert test_printer.input_count == 1
+
+    def test_adds_input_b(self, test_printer):
+        for i in range(2):
+            test_printer.add_input()
+        assert test_printer.inputs['B'] == {'component': None, 'signal': None}
+
+    def test_increments_input_counter_to_2(self, test_printer):
+        for i in range(2):
+            test_printer.add_input()
+        assert test_printer.input_count == 2
 
